a811431fbc9c0bfda9ef9685d798b11a81a76860
George Lioris <george.lioris@gmail.com>
2019-06-19 19:42:27 +0300

    Added itemProp function to render any Item property in cartItems

--- src/components/CartItemList.js
+++ src/components/CartItemList.js
@@ -2,9 +2,11 @@ import React from "react";
 import Icon from "@material-ui/core/Icon";
 
 const subTotal = items => cartItem => {
-  return (
-    items.find(items => cartItem.id === items.id).price * cartItem.quantity
-  );
+  return itemProp(items)(cartItem, "price") * cartItem.quantity;
+};
+
+const itemProp = items => (cartItem, prop) => {
+  return items.find(item => cartItem.id === item.id)[prop];
 };
 
 const CartItem = ({
@@ -13,37 +15,36 @@ const CartItem = ({
   onClickInc,
   onClickDec,
   onClickRemove
-}) => {
-  const itemTitle = ci => {
-    return items.find(i => ci.id === i.id).title;
-  };
-  return (
-    <div className="card grey darken-1" key={cartItem.id}>
-      <div className="card-content white-text">
-        <span className="card-title">
-          {itemTitle(cartItem)}
-          <div className="remove">
-            <span onClick={onClickRemove}>
-              <Icon>clear</Icon>
-            </span>
-          </div>
-        </span>
-
-        <div className="action">
-          <span onClick={onClickDec}>
-            <Icon>remove_circle</Icon>
+}) => (
+  <div className="card grey darken-1" key={cartItem.id}>
+    <div className="card-content white-text">
+      <span className="card-title">
+        {itemProp(items)(cartItem, "title")}
+        <div className="remove">
+          <span onClick={onClickRemove}>
+            <Icon>clear</Icon>
           </span>
-          <span className="quantity">{cartItem.quantity}</span>
-          <span onClick={onClickInc}>
-            <Icon>add_circle</Icon>
-          </span>
-          <br />
-          <span>Subtotal: {subTotal(items)(cartItem)}$</span>
         </div>
+      </span>
+
+      <div className="action">
+        <span onClick={onClickDec}>
+          <Icon>remove_circle</Icon>
+        </span>
+        <span className="quantity">{cartItem.quantity}</span>
+        <span onClick={onClickInc}>
+          <Icon>add_circle</Icon>
+        </span>
+        <br />
+        <span>
+          Subtotal: {subTotal(items)(cartItem)}
+          {` `}$
+        </span>
       </div>
     </div>
-  );
-};
+  </div>
+);
+
 const CartItemList = ({ cartItems, items, IncQuant, DecQuant, RemoveItem }) => (
   <div className="cartList">
     {cartItems.map(cartItem => {
@@ -65,7 +66,7 @@ const CartItemList = ({ cartItems, items, IncQuant, DecQuant, RemoveItem }) => (
         {cartItems.reduce((acc, cartItem) => {
           return acc + subTotal(items)(cartItem);
         }, 0)}
-        $
+        {` `}$
       </span>
     </div>
   </div>
