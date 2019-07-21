3c65e8aa505501a3eede39e1fe19c54f756d01e4
George Lioris <george.lioris@gmail.com>
2019-07-21 16:17:27 +0300

    Added comments on helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,19 +1,23 @@
-export const subTotal = items => cartItem => {
-  return getProperty(items)(cartItem)("price") * cartItem.quantity;
-};
-
+// Returns a (cartItem) (property) from (itemsObj)
 export const getProperty = items => cartItem => prop => {
   const key = cartItem.id;
   return items.hasOwnProperty(key) ? items[key][prop] : "Item Removed";
 };
 
+// Returns a (cartItem) subtotal, getting the price from (itemsObj)
+export const subTotal = items => cartItem => {
+  return getProperty(items)(cartItem)("price") * cartItem.quantity;
+};
+
+// Returns [cartItems] total getting the price from (itemsObj)
 export const total = (cartItems, items) => {
   const total = cartItems.reduce((acc, cartItem) => {
     return acc + subTotal(items)(cartItem);
   }, 0);
   return total ? `Total: ${total} $` : "";
 };
-// Return only unique entires that are not empty strings
+
+// Returns only unique non-empty values from an array
 export const onlyUnique = array => [
   ...array
     .filter((value, index, array) => array.indexOf(value) === index)
