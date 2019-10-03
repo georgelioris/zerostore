bdc47f52a81b0208d3802ae9b7b2e28c8bfdae3d
George Lioris <george.lioris@gmail.com>
2019-10-03 19:44:46 +0300

    added items reducer tests

---
+++ src/__tests__/items_reducer_test.js
@@ -0,0 +1,89 @@
+import items from "../reducers/items";
+import * as types from "../constants";
+
+const initstate = {
+  1: {
+    id: 1,
+    title: "Title",
+    desc: "some desc",
+    price: 110,
+    img: "someUrl",
+    category: "winter",
+    available: true
+  }
+};
+
+describe("items reucer", () => {
+  it("should return the initial state", () => {
+    expect(items(initstate, {})).toEqual(initstate);
+  });
+
+  it("should handle item property changes", () => {
+    expect(
+      items(initstate, {
+        type: types.ITEM_CHANGE,
+        payload: {
+          key: 1,
+          properties: {
+            title: "New Title",
+            price: 42,
+            category: "New CATEGORY"
+          }
+        }
+      })
+    ).toEqual({
+      1: {
+        id: 1,
+        title: "New Title",
+        desc: "some desc",
+        price: 42,
+        img: "someUrl",
+        category: "new category",
+        available: true
+      }
+    });
+  });
+  it("should add an item to shop", () => {
+    expect(
+      items(initstate, {
+        type: types.ADD_TO_SHOP,
+        item: {
+          title: "Added Item Title",
+          desc: "Added Item Desc",
+          price: 42,
+          img: "someUrl",
+          category: "ADDED Category",
+          available: true
+        }
+      })
+    ).toEqual({
+      1: {
+        id: 1,
+        title: "Title",
+        desc: "some desc",
+        price: 110,
+        img: "someUrl",
+        category: "winter",
+        available: true
+      },
+      2: {
+        id: 2,
+        title: "Added Item Title",
+        desc: "Added Item Desc",
+        price: 42,
+        img: "someUrl",
+        category: "added category",
+        available: true
+      }
+    });
+  });
+
+  it("should handle remove item from shop", () => {
+    expect(
+      items(initstate, {
+        type: types.REMOVE_FROM_SHOP,
+        item: { id: 1 }
+      })
+    ).toEqual({});
+  });
+});
