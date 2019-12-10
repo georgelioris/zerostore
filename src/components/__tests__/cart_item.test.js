3e035b68d06fa8488c761b9eb2210b36cc028dbe
George Lioris <george.lioris@gmail.com>
2019-12-10 08:45:40 +0200

    Assigned different names to mock functions to distinguish calls

--- src/components/__tests__/cart_item.test.js
+++ src/components/__tests__/cart_item.test.js
@@ -19,14 +19,16 @@ afterEach(() => {
 });
 
 // Mock state and actions
-const mock = jest.fn();
+const mockInc = jest.fn();
+const mockDec = jest.fn();
+const mockRemove = jest.fn();
 const cartItems = [{ id: 1, quantity: 1 }, { id: 4, quantity: 2 }];
 const props = {
   items: items,
   cartItems: cartItems,
-  incQuant: mock,
-  decQuant: mock,
-  removeFromCart: mock
+  incQuant: mockInc,
+  decQuant: mockDec,
+  removeFromCart: mockRemove
 };
 
 // Tests
@@ -36,7 +38,7 @@ describe("CartItemList component", () => {
     expect(container.toJSON()).toMatchSnapshot();
   });
   it("Should dispatch incQuant/decQaunt on button click", () => {
-    // Click all incQuant and decQaunt buttons
+    // Click all incQuant and decQaunt buttons that are not disabled
     act(() => {
       container.root
         .findAllByProps({
@@ -46,18 +48,19 @@ describe("CartItemList component", () => {
     });
 
     expect(props.incQuant).toHaveBeenCalledTimes(2);
-    expect(props.decQuant).toHaveBeenCalledTimes(2);
+    expect(props.decQuant).toHaveBeenCalledTimes(1);
     expect(props.incQuant).toHaveBeenNthCalledWith(1, { id: 1, quantity: 1 });
     expect(props.incQuant).toHaveBeenNthCalledWith(2, { id: 4, quantity: 2 });
-    expect(props.decQuant).toHaveBeenNthCalledWith(1, { id: 1, quantity: 1 });
-    expect(props.decQuant).toHaveBeenNthCalledWith(2, { id: 4, quantity: 2 });
+    expect(props.decQuant).toHaveBeenCalledWith({ id: 4, quantity: 2 });
   });
   it("Should dispatch removeFromCart on button click", () => {
     // Click all removeFromCart buttons
     act(() => {
-      container.root.findAllByProps({
-        className: "remove"
-      });
+      container.root
+        .findAllByProps({
+          className: "remove"
+        })
+        .map(i => i.props.children.props.onClick());
     });
 
     expect(props.removeFromCart).toHaveBeenCalledTimes(2);
